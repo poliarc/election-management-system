a51c8e760345baa78e62a0bfbc6b493a3d1b06fb
Gaurav2k50 <gaurav2k50@gmail.com>
2025-11-14 18:09:57 +0530

    refactor: comment out unused imports and routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,43 +10,43 @@ import AdminLayout from "./layouts/AdminLayout";
 import StateLayout from "./layouts/StateLayout";
 import DistrictLayout from "./layouts/DistrictLayout";
 import AssemblyLayout from "./layouts/AssemblyLayout";
-import BlockLayout from "./layouts/BlockLayout";
-import MandalLayout from "./layouts/MandalLayout";
-import PollingCenterLayout from "./layouts/PollingCenterLayout";
-import BoothLayout from "./layouts/BoothLayout";
-import KaryakartaLayout from "./layouts/KaryakartaLayout";
+// import BlockLayout from "./layouts/BlockLayout";
+// import MandalLayout from "./layouts/MandalLayout";
+// import PollingCenterLayout from "./layouts/PollingCenterLayout";
+// import BoothLayout from "./layouts/BoothLayout";
+// import KaryakartaLayout from "./layouts/KaryakartaLayout";
 import AdminOverview from "./pages/Admin/Overview";
 import StateOverview from "./pages/State/Dashboard";
 import StateTeamListing from "./pages/State/state-team";
 import StateBlockListing from "./pages/State/block";
 import DistrictDashboard from "./pages/District/Dashboard";
 import DistrictTeam from "./pages/District/DistrictTeam";
-import DistrictAssembly from "./pages/District/Assembly";
-import DistrictBlock from "./pages/District/Block";
-import DistrictMandal from "./pages/District/Mandal";
-import DistrictBooth from "./pages/District/Booth";
-import DistrictPollingCenters from "./pages/District/PollingCenters";
-import DistrictKaryakarta from "./pages/District/Karyakarta";
-import DistrictCampaigns from "./pages/District/Campaigns";
-import DistrictInitiatives from "./pages/District/Initiatives";
-import DistrictProfile from "./pages/District/Profile";
+// import DistrictAssembly from "./pages/District/Assembly";
+// import DistrictBlock from "./pages/District/Block";
+// import DistrictMandal from "./pages/District/Mandal";
+// import DistrictBooth from "./pages/District/Booth";
+// import DistrictPollingCenters from "./pages/District/PollingCenters";
+// import DistrictKaryakarta from "./pages/District/Karyakarta";
+// import DistrictCampaigns from "./pages/District/Campaigns";
+// import DistrictInitiatives from "./pages/District/Initiatives";
+// import DistrictProfile from "./pages/District/Profile";
 // import StateDistrictsListing from "./pages/State/districts";
 import StateAssemblyListing from "./pages/State/assembly";
 // import DistrictOverview from "./pages/District/Overview";
 import StateDistrictsListing from "./pages/State/districts";
 // import StateAssemblyListing from "./pages/State/assembly";
 // import DistrictOverview from "./pages/District/Overview";
-import AssemblyOverview from "./pages/Assembly/Overview";
+// import AssemblyOverview from "./pages/Assembly/Overview";
 import AssemblyDashboard from "./pages/Assembly/Dashboard";
-import BlockOverview from "./pages/Block/Overview";
+// import BlockOverview from "./pages/Block/Overview";
 import AssemblyBlockPage from "./pages/Assembly/block/BlockPage";
 import AssemblyMandalPage from "./pages/Assembly/mandal/MandalPage";
 import AssemblyBoothPage from "./pages/Assembly/booth/BoothPage";
 import AssemblyKaryakartaPage from "./pages/Assembly/karyakarta/KaryakartaPage";
-import MandalOverview from "./pages/Mandal/Overview";
-import PollingCenterOverview from "./pages/PollingCenter/Overview";
-import BoothOverview from "./pages/Booth/Overview";
-import KaryakartaOverview from "./pages/Karyakarta/Overview";
+// import MandalOverview from "./pages/Mandal/Overview";
+// import PollingCenterOverview from "./pages/PollingCenter/Overview";
+// import BoothOverview from "./pages/Booth/Overview";
+// import KaryakartaOverview from "./pages/Karyakarta/Overview";
 import StateMandalListing from "./pages/State/mandal";
 import StateBoothListing from "./pages/State/booth";
 import StateKaryakartaListing from "./pages/State/karyakarta";
@@ -84,22 +84,22 @@ export default function App() {
             <Route index element={<DistrictDashboard />} />
             <Route path="dashboard" element={<DistrictDashboard />} />
             <Route path="district-team" element={<DistrictTeam />} />
-            <Route path="assembly" element={<DistrictAssembly />} />
+            {/* <Route path="assembly" element={<DistrictAssembly />} />
             <Route path="block" element={<DistrictBlock />} />
             <Route path="mandal" element={<DistrictMandal />} />
-            <Route path="booth" element={<DistrictBooth />} />
-            <Route
+            <Route path="booth" element={<DistrictBooth />} /> */}
+            {/* <Route
               path="polling-centers"
               element={<DistrictPollingCenters />}
-            />
-            <Route path="karyakarta" element={<DistrictKaryakarta />} />
-            <Route path="campaigns" element={<DistrictCampaigns />} />
-            <Route path="initiatives" element={<DistrictInitiatives />} />
-            <Route path="profile" element={<DistrictProfile />} />
+            /> */}
+            {/* <Route path="karyakarta" element={<DistrictKaryakarta />} /> */}
+            {/* <Route path="campaigns" element={<DistrictCampaigns />} /> */}
+            {/* <Route path="initiatives" element={<DistrictInitiatives />} /> */}
+            {/* <Route path="profile" element={<DistrictProfile />} /> */}
           </Route>
           <Route path="assembly" element={<AssemblyLayout />}>
-            <Route index element={<AssemblyOverview />} />
-            <Route path="overview" element={<AssemblyOverview />} />
+            {/* <Route index element={<AssemblyOverview />} /> */}
+            {/* <Route path="overview" element={<AssemblyOverview />} /> */}
             <Route path="dashboard" element={<AssemblyDashboard />} />
             <Route path="block" element={<AssemblyBlockPage />} />
             <Route path="block/overview" element={<AssemblyBlockPage />} />
@@ -113,7 +113,7 @@ export default function App() {
               element={<AssemblyKaryakartaPage />}
             />
           </Route>
-          <Route path="block" element={<BlockLayout />}>
+          {/* <Route path="block" element={<BlockLayout />}>
             <Route index element={<BlockOverview />} />
             <Route path="overview" element={<BlockOverview />} />
           </Route>
@@ -132,7 +132,7 @@ export default function App() {
           <Route path="karyakarta" element={<KaryakartaLayout />}>
             <Route index element={<KaryakartaOverview />} />
             <Route path="overview" element={<KaryakartaOverview />} />
-          </Route>
+          </Route> */}
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
